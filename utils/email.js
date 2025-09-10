12d81f219344c9e9dca1010cc4e1ce4e99654591
godwininyene <godwinhigh2@gmail.com>
2025-09-10 15:28:06 +0100

    WIP:Refactor email class

--- utils/email.js
+++ utils/email.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 const pug = require('pug');
 const { convert } = require('html-to-text');
 
-module.exports= class Email{
-    constructor(user, url, type){
+module.exports = class Email {
+    constructor(user, url, type) {
         this.to = user.email;
         this.from = `Vico Exchange <${process.env.EMAIL_FROM}>`;
         this.firstName = user.firstName;
@@ -11,66 +11,66 @@ module.exports= class Email{
         this.type = type
     }
 
-    newTransport(){
-        if(process.env.NODE_ENV === 'production'){
-            // Using Gmail service
-            return nodemailer.createTransport({
-                service:"Gmail",
-                auth:{
-                    user:process.env.GMAIL_USERNAME,
-                    pass:process.env.GMAIL_PASS
-                }
-            })
-        }
-
+    newTransport() {  
         // if (process.env.NODE_ENV === 'production') {
+        //     // Using Gmail service
         //     return nodemailer.createTransport({
-        //         host: process.env.EMAIL_HOST,
-        //         port: process.env.EMAIL_PORT,
-        //         secure: process.env.EMAIL_PORT == 465, // Use TLS for port 465
+        //         service: "Gmail",
         //         auth: {
-        //             user: process.env.EMAIL_USER,
-        //             pass: process.env.EMAIL_PASSWORD
+        //             user: process.env.GMAIL_USERNAME,
+        //             pass: process.env.GMAIL_PASS
         //         }
-        //     });
+        //     })
         // }
+
+        if (process.env.NODE_ENV === 'production') {
+            return nodemailer.createTransport({
+                host: process.env.EMAIL_HOST,
+                port: process.env.EMAIL_PORT,
+                secure: true,
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASSWORD,
+                },
+            });
+        }
     }
 
-    async send(template, subject, templateData={}){ 
+    async send(template, subject, templateData = {}) {
         // 1) Render the HTML base on the pug template
-        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`,{
-            firstName:this.firstName,
-            url:this.url,
-            type:this.type,
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            type: this.type,
             ...templateData
         })
         //2) Define email options
         const mailOptions = {
-            from:this.from,
-            to:this.to,
+            from: this.from,
+            to: this.to,
             subject,
             html,
             text: convert(html, {
                 wordwrap: 130,       // Better formatting for plain text
                 selectors: [
-                { selector: 'a', options: { ignoreHref: true } }, // Cleaner links
-                { selector: 'img', format: 'skip' }               // Skip images in text
+                    { selector: 'a', options: { ignoreHref: true } }, // Cleaner links
+                    { selector: 'img', format: 'skip' }               // Skip images in text
                 ]
             })
         }
         // 3) Create a transport and send email
-       await this.newTransport().sendMail(mailOptions)
+        await this.newTransport().sendMail(mailOptions)
     }
 
 
-    async sendTransaction(transactionData){
+    async sendTransaction(transactionData) {
         await this.send('transaction', 'Transaction Notice', transactionData)
     }
-     async sendOnBoard(){
+    async sendOnBoard() {
         await this.send("welcome", "Account Approval Status")
     }
 
-    async sendTransactionAdmin(transactionData){
+    async sendTransactionAdmin(transactionData) {
         await this.send('transactionAdmin', 'Transaction Notice', transactionData)
     }
 }
